a97c0e856466b0383c74ee70134ebc02dae2539e
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-08-23 15:01:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 import telebot
 from telebot import types
+from num2words import num2words
+from docx import Document
+from docx.shared import Pt
+from docx.oxml.ns import qn
+from docx.oxml import OxmlElement
 from msgs import *
 from t0ken import *
 
 bot = telebot.TeleBot(TOKEN)
 
 print("run in progress...")
+numero = 0
 placeholders = {}
 subs = ["{{номер}}", "{{дата}}", "{{ок_дата}}", "{{стоимость}}", "{{фио}}", "{{нум_ту_вордс}}", "{{ндфл}}",
         "{{ндфл_ту_вордс}}", "{{серияномер}}", "{{код}}", "{{др}}", "{{дата_выд}}", "{{кем}}", "{{адрес}}",
@@ -25,6 +31,14 @@ def start(message):
         print(e)
 
 
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    placeholders[subs[numero]] = message.text
+    print(placeholders[subs[numero]])
+    bot.reply_to(message, 'Записал, нажмите *"Далее"* для продолжения', parse_mode='Markdown', reply_markup=markup)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == "gph")
 def fill(call):
     markup = types.InlineKeyboardMarkup(row_width=1)
@@ -36,223 +50,222 @@ def fill(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == "nomer")
 def nom_dog(call):
+    global numero
+    numero = 0
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Далее', callback_data='date')
     markup.add(item1)
-    bot.send_message(call.message.chat.id, text=nomer,
+    bot.send_message(call.message.chat.id, text="Введите номер договора",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "date")
 def date(call):
-    global placeholders, subs
-    placeholders[subs[0]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[0]])
+    global placeholders, subs, numero
+    numero = 1
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='nomer')
     item2 = types.InlineKeyboardButton('Далее', callback_data='date_ok')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=date,
+    bot.send_message(call.message.chat.id, text='Введите дату заключения договора (в формате\n(в формате дд.мм.гггг))',
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "date_ok")
 def date_ok(call):
-    global placeholders, subs
-    placeholders[subs[1]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[1]])
+    global placeholders, subs, numero
+    numero = 2
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='date')
     item2 = types.InlineKeyboardButton('Далее', callback_data='stoim')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=date_ok,
+    bot.send_message(call.message.chat.id, text='Введите дату окончания действия договора\n(в формате дд.мм.гггг)',
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "stoim")
 def stoim(call):
-    global placeholders, subs
-    placeholders[subs[2]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[2]])
+    global placeholders, subs, numero
+    numero = 3
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='date_ok')
     item2 = types.InlineKeyboardButton('Далее', callback_data='fio')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=stoim,
+    bot.send_message(call.message.chat.id, text="Введите стоимость услуг исполнителя\n(в формате 75000.00)",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "fio")
 def fio(call):
-    global placeholders, subs
-    placeholders[subs[3]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[3]])
+    global placeholders, subs, numero
+    numero = 4
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='stoim')
     item2 = types.InlineKeyboardButton('Далее', callback_data='pasp')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=fio,
+    bot.send_message(call.message.chat.id, text="Введите ФИО исполнителя",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "pasp")
 def pasp(call):
-    global placeholders, subs
-    placeholders[subs[4]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[4]])
+    global placeholders, subs, numero
+    numero = 5
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='fio')
     item2 = types.InlineKeyboardButton('Далее', callback_data='kod')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=pasp,
+    bot.send_message(call.message.chat.id, text="Введите серию, номер паспорта",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "kod")
 def kod(call):
-    global placeholders, subs
-    placeholders[subs[5]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[5]])
+    global placeholders, subs, numero
+    numero = 6
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='pasp')
     item2 = types.InlineKeyboardButton('Далее', callback_data='dr')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=kod,
+    bot.send_message(call.message.chat.id, text="Введите код подразделения",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "dr")
 def dr(call):
-    global placeholders, subs
-    placeholders[subs[6]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[6]])
+    global placeholders, subs, numero
+    numero = 7
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='kod')
     item2 = types.InlineKeyboardButton('Далее', callback_data='vyd')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=dr,
+    bot.send_message(call.message.chat.id, text="Введите дату роджения\n(в формате дд.мм.гггг)",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "vyd")
 def vyd(call):
-    global placeholders, subs
-    placeholders[subs[7]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[7]])
+    global placeholders, subs, numero
+    numero = 8
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='dr')
     item2 = types.InlineKeyboardButton('Далее', callback_data='kem')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=vyd,
+    bot.send_message(call.message.chat.id, text="Введите дату выдачи паспорта\n(в формате дд.мм.гггг)",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "kem")
 def kem(call):
-    global placeholders, subs
-    placeholders[subs[8]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[8]])
+    global placeholders, subs, numero
+    numero = 9
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='vyd')
     item2 = types.InlineKeyboardButton('Далее', callback_data='adr')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=kem,
+    bot.send_message(call.message.chat.id, text="Введите кем выдан паспорт",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "adr")
 def adr(call):
-    global placeholders, subs
-    placeholders[subs[9]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[9]])
+    global placeholders, subs, numero
+    numero = 10
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='kem')
     item2 = types.InlineKeyboardButton('Далее', callback_data='rs')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=adr,
+    bot.send_message(call.message.chat.id, text="Введите адрес прописки исполнителя",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "rs")
 def rs(call):
-    global placeholders, subs
-    placeholders[subs[10]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[10]])
+    global placeholders, subs, numero
+    numero = 11
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='adr')
     item2 = types.InlineKeyboardButton('Далее', callback_data='bank')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=rs,
+    bot.send_message(call.message.chat.id, text="Введите рассчетный счет",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "bank")
 def bank(call):
-    global placeholders, subs
-    placeholders[subs[11]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[11]])
+    global placeholders, subs, numero
+    numero = 12
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='rs')
     item2 = types.InlineKeyboardButton('Далее', callback_data='bik')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=bank,
+    bot.send_message(call.message.chat.id, text="Введите название банка исполнителя",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "bik")
 def bik(call):
-    global placeholders, subs
-    placeholders[subs[12]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[12]])
+    global placeholders, subs, numero
+    numero = 13
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='bank')
     item2 = types.InlineKeyboardButton('Далее', callback_data='mail')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=bik,
+    bot.send_message(call.message.chat.id, text="Введите БИК",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "mail")
 def mail(call):
-    global placeholders, subs
-    placeholders[subs[13]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[13]])
+    global placeholders, subs, numero
+    numero = 14
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='bik')
     item2 = types.InlineKeyboardButton('Далее', callback_data='tel')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=mail,
+    bot.send_message(call.message.chat.id, text="Введите эл. почту исполнителя",
                      parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "tel")
 def tel(call):
-    global placeholders, subs
-    placeholders[subs[14]] = call.message.text
-    print(call.message.text, '\tvs\t', placeholders[subs[14]])
+    global placeholders, subs, numero
+    numero = 15
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('Назад', callback_data='mail')
     item2 = types.InlineKeyboardButton('Готово!', callback_data='send')
     markup.add(item1, item2)
-    bot.send_message(call.message.chat.id, text=tel,
+    bot.send_message(call.message.chat.id, text="Введите номер телефона исполнителя\n(в формате 7 (9xx) xxx xx xx)",
                      parse_mode='Markdown', reply_markup=markup)
 
+bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id)
 
 @bot.callback_query_handler(func=lambda call: call.data == "send")
-def docx(call):
+def docx(callback_query):
     try:
-        global placeholders, subs
-        placeholders[subs[-1]] = call.message.text
-        print(call.message.text, '\tvs\t', placeholders[subs[-1]])
+        global placeholders
         placeholders["{{нум_ту_вордс}}"] = num2words(int(placeholders["{{стоимость}}"]), lang='ru')
-        placeholders["{{ндфл}}"] = 0.13 * int(placeholders["{{стоимость}}"])
+        placeholders["{{ндфл}}"] = str(0.13 * int(placeholders["{{стоимость}}"]))
         placeholders["{{ндфл_ту_вордс}}"] = num2words(int(placeholders["{{ндфл}}"]), lang='ru')
         secondname, firstname, otch = placeholders["{{фио}}"].split()
         placeholders["{{фио_кор}}"] = firstname[0] + '. ' + otch[0] + '. ' + secondname
 
         print(placeholders)
+        doc = Document('Договор ГПХ.docx')
 
-        target_text = placeholders["{{фио}}"]
+        for paragraph in doc.paragraphs:
+            for placeholder, value in placeholders.items():
+                if placeholder in paragraph.text:
+                    # Заменяем текст и устанавливаем шрифт
+                    if value:
+                        paragraph.text = paragraph.text.replace(placeholder, value)
+                        run = paragraph.runs[0]
+                        font = run.font
+                        font.name = "Times New Roman"
+                        font.size = Pt(12)
+
+        target_text = "Гражданин(ка) Российской Федерации " + placeholders["{{фио}}"]
+
         for paragraph in doc.paragraphs:
             if target_text in paragraph.text:
                 run = paragraph.runs[0]  # Берем первую текстовую руну параграфа
@@ -276,8 +289,8 @@ def docx(call):
                     font.size = Pt(12)
                     # Добавляем текст после выделенной части без жирного начертания
                     # new_run.add_text(parts[1])
-                    doc.save('ГПХ' + placeholders["{{фио_кор}}"] + '.docx')
-        document_path = 'ГПХ' + placeholders["{{фио_кор}}"] + '.docx'
+                    doc.save('Договор ГПХ.docx')
+        document_path = 'Договор ГПХ.docx'
         with open(document_path, 'rb') as docx_file:
             bot.send_document(callback_query.message.chat.id, docx_file)
     except Exception as e:
